9efb914baa8b33b865a5459a0849bf5a88640ffe
CHENYUN ZHANG <morningchenyun@gmail.com>
2020-07-17 12:54:07 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -2,7 +2,7 @@ def find_element_index(array, value_to_find)
   count=0
   while count<array.length do
     if array[count]== value_to_find
-      retunr count
+      return count
     end
     count+=1
   end
